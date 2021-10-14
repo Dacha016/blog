d93a31cf4f2c2c8de860db43bee1ba49605296c7
Dalibor Marinkovic <dalibor.marinkovic@gmail.com>
2021-10-14 21:12:33 +0200

    12 video

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,20 +3,13 @@
     <head>
         <title>Blog</title>
         <link rel="stylesheet" href="/app.css">
-       
+
     </head>
     <body>
+        <?php foreach($posts as $post): ?>
        <article>
-           <a href="posts/my-first-post"><h1>My first post</h1></a>
-           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque sapiente sunt hic nulla qui dolor modi explicabo labore perspiciatis ex libero optio ea, nemo doloremque ullam voluptatibus. Sint, libero est.</p>
+           <?= $post;?>
        </article>
-       <article>
-       <a href="posts/my-second-post"><h1>My second post</h1></a>
-           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque sapiente sunt hic nulla qui dolor modi explicabo labore perspiciatis ex libero optio ea, nemo doloremque ullam voluptatibus. Sint, libero est.</p>
-       </article>
-       <article>
-       <a href="posts/my-third-post"><h1>My third post</h1></a>
-           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque sapiente sunt hic nulla qui dolor modi explicabo labore perspiciatis ex libero optio ea, nemo doloremque ullam voluptatibus. Sint, libero est.</p>
-       </article>
+      <?php endforeach; ?>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 use App\Models\Post;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,12 +17,26 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('posts');
+    // $files= File::files(resource_path("posts"));
+    // $posts=[];
+    // foreach($files as $file){
+    //     $document=YamlFrontMatter::parseFile($file);
+    //     $posts[]= new Post(
+    //         $document->title,
+    //         $document->excerpt,
+    //         $document->date,
+    //         $document->body
+    //     );
+    // }
+    $document=YamlFrontMatter::parseFile(resource_path("posts/my-first-post.html"));
+    ddd($document);
+       return view('posts',[
+        "posts"=> Post::all()
+    ]);
 });
 Route::get("posts/{post}",function($slug){
-    $post= Post::find($slug);
     return view("post",[
-            "post"=> $post
+            "post"=> Post::find($slug)
         ]);
 
 
