896e4887966335e956f58f25d9d385a6b418967b
Dalibor Marinkovic <dalibor.marinkovic@gmail.com>
2021-10-19 16:14:12 +0200

    29 video

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -8,8 +8,8 @@
      <a href="/posts/{{$post->slug; }} ">{{$post->title}}</a>
     </h1>
     <p>
-        <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a>
-    </p>
+        By <a href="/authors/{{$post->author->userName}}">{{$post->author->name}}</a> in <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a>
+      </p>
    <div>
        {{$post->excerpt}}
    </div>
